012a7c0538730fe96540154c1ba7aea21a384e1a
Santhosh Kumar P <tony.antonystark123@gmail.com>
2024-01-11 18:35:36 +0530

    product delete

--- controllers/groceryProductsController.js
+++ controllers/groceryProductsController.js
@@ -23,10 +23,36 @@ const getAllProducts=async(req,res)=>{
   res.status(200).json({products})
   }catch(error)
   {
-    res.status(400).json({error})
+    res.status(400).json({})
+  }
+}
+
+
+const deleteProduct=async(req,res)=>{
+
+  const {id}=req.params;
+  console.log(id)
+
+  try{
+
+    const deletedItem=await groceryProductModel.findByIdAndDelete(id);
+    
+    if(deletedItem)
+    {
+      res.status(200).json(deletedItem)
+    
+    }
+    else
+    {
+      res.status(404).json({ success: false, message: 'Item not found' });
+    }
+
+  }catch(error)
+  {
+    res.status(404).json({message:error.message});
   }
 
 
 }
 
-module.exports = { addNewProduct ,getAllProducts};
+module.exports = { addNewProduct ,getAllProducts,deleteProduct};
--- routes/groceryRouter.js
+++ routes/groceryRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const {getAllCategory,addNewCategory}=require("../controllers/groceryCategoryController.");
-const {addNewProduct,getAllProducts}=require("../controllers/groceryProductsController");
+const {addNewProduct,getAllProducts, deleteProduct}=require("../controllers/groceryProductsController");
 const router = express.Router();
 
 router.route("/category").get(getAllCategory).post(addNewCategory);
 router.route("/products").post(addNewProduct).get(getAllProducts);
+router.route("/product/:id").delete(deleteProduct);
 
 module.exports = router;
